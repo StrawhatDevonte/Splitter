e2c0470aac8d165e64698639aea08a4dba07557f
Devonte <devontepennamon@gmail.com>
2022-02-26 00:22:55 -0500

    Changed button elements to link elements for easier styling. Started on the javascript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function totalTip(bill, percent) {
+  let tip = bill * percent;
+  return tip;
+}
+
+function tipPerPerson(people) {
+  let tipSplit = totalTip() / people;
+  return tipSplit;
+}
+
+function totalPerPerson(bill, people) {
+  let total = bill / people;
+  return total;
+}
